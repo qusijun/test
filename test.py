30c8ca26cb3f024ae10eaef60f7939a897a02928
qusijun <enterspace1992@gmail.com>
2017-01-11 15:51:03 +0800

    test

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+# File Name: test.py
+# Author:qusijun
+# Mail:wl_lian@yahoo.com
+# Created Time: 四 11/12 11:28:24 2015
+##############################################################
+
+
+def test():
+    return 1
+
+print test()
